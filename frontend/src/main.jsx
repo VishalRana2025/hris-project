2dd64fe03afdb7a2a00cf18caa2a58c2457a85cd
Vishal Rana <vishal.rana@eclipticinsight.com>
2026-04-28 21:29:28 +0530

    fix: browser router setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // ✅ ADD THIS
+import { BrowserRouter } from "react-router-dom"; // ✅ CHANGE THIS
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
