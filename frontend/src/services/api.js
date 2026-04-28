fc1af1f45293b085023256fa82f4fb595181b551
Vishal Rana <vishal.rana@eclipticinsight.com>
2026-04-28 23:57:49 +0530

    final fix: employee system working

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 /* 🔥 BASE INSTANCE */
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`
+  baseURL: "https://hris-project-hw7k.onrender.com/api"
 });
 
 /* 🔥 COMMON AUTH HEADER */
